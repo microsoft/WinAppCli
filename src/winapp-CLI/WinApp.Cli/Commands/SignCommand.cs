b3eb0a35f8128c9da2d6081913ad5b770e68e745
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2026-02-20 12:52:25 -0800

    Update src/winapp-CLI/WinApp.Cli/Commands/SignCommand.cs
    Co-authored-by: Alexandre Zollinger Chohfi <alzollin@microsoft.com>

--- src/winapp-CLI/WinApp.Cli/Commands/SignCommand.cs
+++ src/winapp-CLI/WinApp.Cli/Commands/SignCommand.cs
@@ -64,7 +64,7 @@ internal class SignCommand : Command
                 }
                 catch (InvalidOperationException error)
                 {
-                    return (1, $"{error.Message}");
+                    return (1, error.Message);
                 }
                 catch (Exception error)
                 {
