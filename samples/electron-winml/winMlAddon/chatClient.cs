324da989cf6edc50dbc0ef4d2a029490df9ec1fa
Jörmungandrk <github@zerodaysec.org>
2026-02-27 02:49:35 +0700

    Fix Rethrowing exception variable dereferenced variable may be null

--- samples/electron-winml/winMlAddon/chatClient.cs
+++ samples/electron-winml/winMlAddon/chatClient.cs
@@ -73,7 +73,7 @@ public class ChatClient
         {
             if (cts != null && !cts.Token.IsCancellationRequested)
             {
-                throw ex;
+                throw;
             }
         }
 
