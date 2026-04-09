12faff44f4f1f5d07fa23964b35eb2cf9ee8472b
Zachary Teutsch <zteutsch@microsoft.com>
2026-04-09 16:22:40 -0400

    fix tests

--- scripts/package-npm.ps1
+++ scripts/package-npm.ps1
@@ -200,7 +200,7 @@ try
     Write-Host "[PACK] Creating npm package tarball..." -ForegroundColor Blue
     
     # Calculate relative path from npm project to output directory
-    $RelativeOutputPath = [System.IO.Path]::GetRelativePath($NpmProjectPath, $OutputPath)
+    $RelativeOutputPath = Resolve-Path -Relative $OutputPath
     
     npm pack --pack-destination $RelativeOutputPath
     $PackResult = $LASTEXITCODE
